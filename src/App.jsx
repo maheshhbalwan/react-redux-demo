a177b2c9441cbbc3f2111f1db3e57723bcfbb2e5
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-06-13 12:28:27 +0530

    add: counters
    increment and decrement counters

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold text-center">Hello world!</h1>;
+import React from "react";
+import Counter from "./components/Count";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center gap-4 ">
+      <button
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 m-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Increment
+      </button>
+      <Counter />
+      <button
+        type="button"
+        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 m-2  dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+      >
+        Decrement
+      </button>
+    </div>
+  );
 }
+
+export default App;
---
+++ src/components/Count.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      {" "}
+      <h1 className="font-semibold">0</h1>
+    </div>
+  );
+};
+
+export default Counter;
