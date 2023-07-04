1172538f16bbed03a9c0abbdc3c702d6bbfc7aae
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-07-04 23:02:33 +0530

    deploy on GitHub pages using GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "react-redux-demo",
   server: {
     port: 3000,
   },
